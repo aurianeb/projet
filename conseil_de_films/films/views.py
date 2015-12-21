ae7c5847b6c0d6e7e619ed311fd103391f52bef7
Margot_B <barbet.margot@yahoo.fr>
2015-12-21 16:35:53 +0100

    fonction pour ne garder que les choix effectues

--- conseil_de_films/films/views.py
+++ conseil_de_films/films/views.py
@@ -3,6 +3,7 @@ import xlrd
 # Create your views here.
 
 #A partir du tableau excel creation d'un tableau python pour la base de donnees
+
 base = xlrd.open_workbook('Base_films.xls')
 tab = base.sheet_by_name(u'Feuil1')
 titre_original = tab.col_values(0)
@@ -19,3 +20,30 @@ scenario = tab.col_values(10)
 origine = tab.col_values(11)
 photographie = tab.col_values(12)
 musique = tab.col_values(13)
+
+#Supprimer une ligne dans le tableau
+def supprimer_ligne(i) :
+    del(titre_original[i])
+    del(titre_francais[i])
+    del(realisateur[i])
+    del(couleur[i])
+    del(annee[i])
+    del(pays[i])
+    del(genre[i])
+    del(acteurs[i])
+    del(actrices[i])
+    del(appreciation[i])
+    del(scenario[i])
+    del(origine[i])
+    del(photographie[i])
+    del(musique[i])
+   
+#Fonction qui permet de garder uniquement les films d'un certain type (ex : fantastique)
+def selection(categorie, choix) : # ex : categorie = genre, choix = western
+    for i in range(len(categorie)-1,0,-1) :
+        if categorie[i] != choix :
+            supprimer_ligne(i)
+ 
+#exemple de qql qui n'aime que les films US           
+selection(pays, 'USA') #Attention dans le tableau souvent c'est " USA" donc il faut enlever l'espace pour que ça marche
+#Attention dans le tableau souvent c'est " USA" donc il faut enlever l'espace pour que ça marche

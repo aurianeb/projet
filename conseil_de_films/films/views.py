af4b74c4fc0ff1379ed346d7c3f6b20645071a04
Margot_B <barbet.margot@yahoo.fr>
2015-12-27 10:28:01 +0100

    creation du template

--- conseil_de_films/films/views.py
+++ conseil_de_films/films/views.py
@@ -16,6 +16,5 @@ def recuperation_donnes(request):
     #for i in range(1,len(tab.col_values(0))):
     for i in range(1,5): # Pour le moment avec 5 sinon c'est beaucoup trop long
         Films(titre_original=tab.col_values(0)[i], titre_francais=tab.col_values(1)[i], realisateur=tab.col_values(2)[i], couleur=tab.col_values(3)[i], annee=tab.col_values(4)[i], pays=tab.col_values(5)[i], genre=tab.col_values(6)[i], acteurs=tab.col_values(7)[i], actrices=tab.col_values(8)[i], appreciation=tab.col_values(9)[i], scenario=tab.col_values(10)[i], provenance = tab.col_values(11)[i], photographie = tab.col_values(12)[i], musique = tab.col_values(13)[i]).save()
-    
-    text = """<h1> Pour l'instant ca marche ;) </h1>"""
-    return HttpResponse(text)
+        
+    return render(request, 'films/choix.html', locals())

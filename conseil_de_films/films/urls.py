d02847c7df5223cf25ba80b362172134ffe247f7
Margot_B <barbet.margot@yahoo.fr>
2016-01-01 17:36:02 +0000

    Creation de la base de donnees sur Django et de la fonction pour gérer le choix dans une certaine categorie

--- conseil_de_films/films/urls.py
+++ conseil_de_films/films/urls.py
@@ -8,5 +8,6 @@ Created on Sat Dec 26 13:42:26 2015
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('films.views',
-    url(r'^accueil', 'recuperation_donnes'),
+    url(r'^accueil/(?P<categorie>\w+)/(?P<choix>\w+)/$', 'preference'),
+    url(r'^data_base/','create_DB') # ne faire qu'une fois au début (je l'ai fait!)
 )
--- conseil_de_films/films/views.py
+++ conseil_de_films/films/views.py
@@ -7,14 +7,39 @@ from films.models import Films
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def recuperation_donnes(request):
+
+def create_DB(request):
     
     #A partir du tableau excel creation d'un tableau python pour la base de donnees
     base = xlrd.open_workbook('films/Base_films.xls')
     tab = base.sheet_by_name(u'Feuil1')
+    for i in range(1,2855): #fin du tableau à 2855
+        nouveau_film = Films(titre_original=tab.col_values(0)[i], 
+                             titre_francais=tab.col_values(1)[i],
+                             realisateur=tab.col_values(2)[i],
+                             couleur=tab.col_values(3)[i],
+                             annee=tab.col_values(4)[i],
+                             pays=tab.col_values(5)[i], 
+                             genre=tab.col_values(6)[i], 
+                             acteurs=tab.col_values(7)[i], 
+                             actrices=tab.col_values(8)[i], 
+                             appreciation=tab.col_values(9)[i], 
+                             scenario=tab.col_values(10)[i], 
+                             provenance=tab.col_values(11)[i], 
+                             photographie=tab.col_values(12)[i], 
+                             musique=tab.col_values(13)[i])
+        nouveau_film.save()
+        
+    return render(request, 'films/data_base.html')
 
-    #for i in range(1,len(tab.col_values(0))):
-    for i in range(1,5): # Pour le moment avec 5 sinon c'est beaucoup trop long
-        Films(titre_original=tab.col_values(0)[i], titre_francais=tab.col_values(1)[i], realisateur=tab.col_values(2)[i], couleur=tab.col_values(3)[i], annee=tab.col_values(4)[i], pays=tab.col_values(5)[i], genre=tab.col_values(6)[i], acteurs=tab.col_values(7)[i], actrices=tab.col_values(8)[i], appreciation=tab.col_values(9)[i], scenario=tab.col_values(10)[i], provenance = tab.col_values(11)[i], photographie = tab.col_values(12)[i], musique = tab.col_values(13)[i]).save()
+
+def preference(request, categorie, choix):
+    
+    L=[]
+    my_filter = {}
+    my_filter[categorie] = choix 
+    ma_liste = Films.objects.filter(**my_filter)
+    for film in ma_liste:
+        L.append(film.titre_original)
         
     return render(request, 'films/choix.html', locals())
